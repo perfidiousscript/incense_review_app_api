59cd10f72df1d3861c4dafa7857e34324b52d405
Sam Moss <sam.moss321@gmail.com>
2019-02-10 10:59:19 -0800

    brand, line, incense routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :brands do
+    resources :lines, only: [:index, :new, :create] do
+      resources :incenses, only: [:index, :new, :create]
+      get 'approve', to: 'lines#approve'
+    end
+  end
+  resources :lines, only: [:show, :edit, :update, :destroy]
+  resources :incenses, only: [:show, :edit, :update, :destroy]
+  get '/brands/:id/approve', to: 'brands#approve'
+
 end
