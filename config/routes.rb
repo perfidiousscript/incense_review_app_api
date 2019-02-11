1c247600f2f484ca277d5d79d249aa3a09ef2368
Sam Moss <sam.moss321@gmail.com>
2019-02-11 11:00:12 -0800

    converts routes to use name as param

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  resources :brands do
-    resources :lines, only: [:index, :new, :create] do
-      resources :incenses, only: [:index, :new, :create]
+  resources :brands, param: :name do
+    resources :lines, param: :name, only: [:index, :new, :create] do
+      resources :incenses, param: :name, only: [:index, :new, :create]
       get 'approve', to: 'lines#approve'
     end
   end
-  resources :lines, only: [:show, :edit, :update, :destroy]
-  resources :incenses, only: [:show, :edit, :update, :destroy]
-  get '/brands/:id/approve', to: 'brands#approve'
-
+  resources :lines, param: :line_name, only: [:show, :edit, :update, :destroy]
+  resources :incenses, param: :name, only: [:show, :edit, :update, :destroy]
+  get '/brand/:name/approve', to: 'brands#approve'
 end
