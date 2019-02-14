408e9d17dcbeac7b55cb8640bd89ad18cac8cc85
Sam Moss <sam.moss321@gmail.com>
2019-02-14 15:06:59 -0800

    adds review routes to routes config

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :brands, param: :name do
     resources :lines, param: :name do
-      resources :incenses, param: :name
+      resources :incenses, param: :name do
+        resources :reviews, shallow: true
+      end
       patch 'approve', to: 'lines#approve'
     end
   end
