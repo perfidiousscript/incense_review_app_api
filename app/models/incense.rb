8f98d726004424f57f5ba464e828ded6c84ecf24
Sam Moss <sam.moss321@gmail.com>
2019-02-22 11:59:23 -0800

    compile_statistics moved to Incense model, adds statistics_pre_hash assembler

--- app/models/incense.rb
+++ app/models/incense.rb
@@ -5,4 +5,17 @@ class Incense < ApplicationRecord
   has_many :reviews
   validates :name, presence: true
   validates_uniqueness_of :name, scope: :line_id
+
+  INCENSE_ATTRIBUTES = [:sweet, :sour, :salty, :bitter, :umami, :spice, :earthy, :woody, :minutes, :season].freeze
+
+  def compile_statistics
+    statistics_pre_hash = Hash[INCENSE_ATTRIBUTES.collect { |v| [v,[]] }]
+    statistics_hash = {}
+    reviews.each do |review|
+      statistics_pre_hash.each do |key,value|
+        statistics_pre_hash[key].push(review["#{key}"])
+      end
+    end
+    puts "#{statistics_pre_hash}"
+  end
 end
