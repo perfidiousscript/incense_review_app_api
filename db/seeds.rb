fc5c0042b3616d6527f85f6a1c8653a6e39efa24
Sam Moss <sam.moss321@gmail.com>
2019-03-03 20:39:25 -0800

    fills in db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+brands = Brand.create(
+  [
+    {
+      name: 'shoyeido',
+      country: 'Japan',
+      approved: true,
+      description: 'A very famous Japanese incense brand.'
+    },
+    {
+      name: 'baieido',
+      country: 'Japan',
+      approved: false,
+      description: 'A very old Japanese incense brand.'
+    }
+  ]
+)
+
+lines = Line.create(
+  [
+    {
+      name: 'daily',
+      brand_id: brand.first,
+      approved: true,
+      description: 'Good for day to day use'
+    },
+    {
+      name: 'premium',
+      brand_id: brand.first,
+      approved: false,
+      description: "Shoyeido's high end incense line"
+    }
+  ]
+)
+
+incenses = Incense.create(
+  [
+    {
+      name: 'golden pavilion'.
+      line_id: lines.first,
+      description: 'A spicy, earthy daily incense'
+    },
+    {
+      name: 'white cloud'.
+      line_id: lines.first,
+      description: 'A great, aloeswood based daily incense'
+    }
+  ]
+)
